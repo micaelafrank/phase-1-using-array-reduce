31cbd4dbc0f5dcb194c088439cf7c0dbd73ae314
Micaela Frank <micaelalfrank@gmail.com>
2022-04-29 09:43:30 -0400

    commit tree 1397f5f996f74db4f55dd9ba81f7cecc82d02e5a

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
 // Code your solution here
+
+// function getTotal(array){
+//     for(element of array){
+//         totalBatteries += element;
+//     }
+// return totalBatteries;
+// }
+
+const totalBatteries = batteryBatches.reduce(function(addBatteries, battery){
+    let sum = addBatteries +=battery;
+    console.log(`Your total sum is: ${sum}.`);
+    return sum;
+});
+
+totalBatteries;
